e1d1046760ecc4b277d2e7a361702d1546f7fd7a
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-07-09 11:55:29 -0500

    String manipulation

--- main.js
+++ main.js
@@ -11,3 +11,15 @@ var str = 'the quieter you become, ';
 var str2 = ' the more you are able to hear';
 let fullStr = str + str2;
 console.log(fullStr);
+
+
+var string = 'New String';
+let splitString = string.split("");
+console.log(splitString);
+
+let joinedString = splitString.join("");
+console.log(joinedString);
+
+var firstName = "KZ011";
+var lastLetterOfFirstName = firstName[firstName.length]; // 1
+console.log(lastLetterOfFirstName);
