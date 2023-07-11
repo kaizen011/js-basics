ebd915c96ef01ad97d593126cbfe694070fd5013
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-07-11 08:43:32 -0500

    Brushed up knowledge

--- main.js
+++ main.js
@@ -106,3 +106,19 @@ var number = [1,2,3,4,5,6,7,8,9,10];
 var returnedMax = findMax(number);
 console.log(returnedMax);
 console.table(returnedMax);
+
+
+function isPrime(num){
+    if (number <= 1){
+        return false;
+    }
+
+    for(var i = 2; i < num; i++){
+        if(num % i === 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
