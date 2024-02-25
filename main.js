382edb43605e91c57c6ed3f33afdbdb5aa31eef4
Elioni H <125158784+kaizen011@users.noreply.github.com>
2024-02-25 21:21:15 +0100

    New exercises on main.js

--- main.js
+++ main.js
@@ -20,6 +20,13 @@ var firstName = "KZ011";
 var lastLetterOfFirstName = firstName[firstName.length -1]; // 1
 
 
+const bifurcate = (arr, filter) =>
+  arr.reduce((acc, val, i) => (acc[filter[i] ? 0 : 1].push(val), acc), [[], []]);
+console.log(bifurcate([1, 2, 3, 4], [true, true, false, true]));
+console.log(bifurcate([1, 2, 3, 4], [true, true, true, true]));
+console.log(bifurcate([1, 2, 3, 4], [false, false, false, false]));
+
+
 const person = {
     name: "Forrest Knight",
     age: 21,
