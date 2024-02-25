c9cb5435b5b8068467ae64b57add5713beb7b481
Elioni H <125158784+kaizen011@users.noreply.github.com>
2024-02-25 21:21:39 +0100

    New exercise on main.js

--- main.js
+++ main.js
@@ -124,6 +124,25 @@ function isPrime(num){
     return true;
 }
 
+function validate_String_Not_Empty(str) {
+  if (str.length === 0) {
+    throw new Error('Sorry, the string is empty!');
+  }
+}
+
+// Example:
+try {
+  const string1 = 'Hello, World!';
+  validate_String_Not_Empty(string1); // Valid non-empty string
+
+  const string2 = '';
+  validate_String_Not_Empty(string2); // Throws an error
+} catch (error) {
+  console.log('Error:', error.message);
+}
+
+
+
 let funModule = (function () {
     return {
       isCuteMixin: function (obj) {
