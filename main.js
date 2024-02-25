8e5d2faaf339ba1931cd78e1fa37045de6df3caa
Elioni H <125158784+kaizen011@users.noreply.github.com>
2024-02-25 21:22:21 +0100

    Error handling - new exercise -> main.js

--- main.js
+++ main.js
@@ -124,6 +124,30 @@ function isPrime(num){
     return true;
 }
 
+
+
+
+
+
+
+function validateArrayNotEmpty(arr) {
+  if (arr.length === 0) {
+    throw new Error('Array is empty!');
+  }
+}
+
+
+try {
+  const nums1 = [1, 2, 3, 4];
+  validateArrayNotEmpty(nums1); 
+
+  const nums2 = [];
+  validateArrayNotEmpty(nums2); 
+} catch (error) {
+  console.log('Error:', error.message);
+}
+
+
 function validate_String_Not_Empty(str) {
   if (str.length === 0) {
     throw new Error('Sorry, the string is empty!');
