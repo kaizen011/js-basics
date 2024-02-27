57fed43f93b45b5fca887e76c9f1a2ff04a1e37b
Elioni H <125158784+kaizen011@users.noreply.github.com>
2024-02-27 22:02:57 +0100

    Updated, new exercise main.js

--- main.js
+++ main.js
@@ -156,10 +156,13 @@ function isPrime(num){
     return true;
 }
 
-
-
-
-
+function remove_from_left(arr, n = 1){
+  return arr.slice(n); 
+}
+console.log(remove_from_left([1, 2, 3]));
+console.log(remove_from_left([1, 2, 3], 1));
+console.log(remove_from_left([1, 2, 3], 2));
+console.log(remove_from_left([1, 2, 3], 4));
 
 
 function validateArrayNotEmpty(arr) {
